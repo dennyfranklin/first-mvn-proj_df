bf8f8cfaa41883576f23e445f1c93cb727fb3839
Denny Franklin <116251491+dennyfranklin@users.noreply.github.com>
2023-06-23 10:25:47 +0530

    first maven project hello world

---
+++ src/main/java/com/fssa/df/sampleprogs/HelloWorldProg.java
@@ -0,0 +1,10 @@
+package com.fssa.df.sampleprogs;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("Hello world");
+	}
+
+}
